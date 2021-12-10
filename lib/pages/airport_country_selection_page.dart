93c00355dee689a4d26c046ea2e1c65bf97e7677
tombortniak <tombortniak@gmail.com>
2021-12-10 19:48:09 +0100

    Add airport country and airport selection pages

--- lib/pages/airport_country_selection_page.dart
+++ lib/pages/airport_country_selection_page.dart
@@ -4,6 +4,7 @@ import 'dart:collection';
 import 'package:emoji_flag_converter/emoji_flag_converter.dart';
 import 'package:travel_finder/models/airport.dart';
 import 'package:travel_finder/models/country.dart';
+import 'package:travel_finder/pages/airport_selection_page.dart';
 
 class AirportCountrySelectionPage extends StatelessWidget {
   final List<Airport> _airports;
@@ -29,7 +30,7 @@ class AirportCountrySelectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      bottom: false,
+      top: false,
       child: Container(
         color: Colors.white,
         child: Column(
@@ -47,15 +48,26 @@ class AirportCountrySelectionPage extends StatelessWidget {
                     tiles: List.generate(
                       _airportsByCountry.keys.toList().length,
                       (index) => ListTile(
-                        leading: Text(
-                          '${EmojiConverter.fromAlpha2CountryCode(_airportsByCountry.keys.toList()[index].alpha2Code)}',
-                          style: TextStyle(fontSize: 40.0),
-                        ),
-                        title: Text(
-                          '${_airportsByCountry.keys.toList()[index].name} ',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      ),
+                          leading: Text(
+                            '${EmojiConverter.fromAlpha2CountryCode(_airportsByCountry.keys.toList()[index].alpha2Code)}',
+                            style: TextStyle(fontSize: 40.0),
+                          ),
+                          title: Text(
+                            '${_airportsByCountry.keys.toList()[index].name}',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => AirportSelectionPage(
+                                        countryName: _airportsByCountry.keys
+                                            .toList()[index]
+                                            .name,
+                                        airports: _airportsByCountry[
+                                            _airportsByCountry.keys
+                                                .toList()[index]])));
+                          }),
                     ),
                   ).toList(),
                 ),
--- lib/pages/airport_selection_page.dart
+++ lib/pages/airport_selection_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:travel_finder/models/airport.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class AirportSelectionPage extends StatelessWidget {
-  const AirportSelectionPage({Key? key}) : super(key: key);
+  final String _countryName;
+  final List<Airport> _airports;
+
+  AirportSelectionPage({required countryName, required airports})
+      : _countryName = countryName,
+        _airports = airports;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      top: false,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            AppBar(
+              title: Text(_countryName),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: ListView(
+                  shrinkWrap: true,
+                  controller: ModalScrollController.of(context),
+                  children: ListTile.divideTiles(
+                    context: context,
+                    tiles: List.generate(
+                      _airports.length,
+                      (index) => ListTile(
+                          title: Text(
+                            '${_airports[index].name}',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          subtitle: Text('${_airports[index].iataCode}')),
+                    ),
+                  ).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
